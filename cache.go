d9e8ec44891ffdfaf27ea42581b64a6fae0dca1e
Diego.Plascencia <diego.plascencia@nordstrom.com>
2022-06-02 12:58:15 -0500

    first-attempt hw5.2

--- cache.go
+++ cache.go
@@ -1,23 +1,61 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+var infiniteDeadline time.Time = time.Date(3000, 12, 30, 23, 59, 59, 100, time.Local)
 
 type Cache struct {
+	cachedValues map[string]string
+	deadlines    map[string]time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cachedValues: map[string]string{},
+		deadlines:    map[string]time.Time{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) { // comparar fechas y no desplegar si se venció
+	value, ok := c.cachedValues[key]
+	keyDeadline := c.deadlines[key]
+	if ok {
+		now := time.Now()
+		if now.Before(keyDeadline) {
+			return value, true
+		} else {
+			delete(c.cachedValues, key)
+			delete(c.deadlines, key)
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) { //agregar timeline infinito
+	c.cachedValues[key] = value
+	c.deadlines[key] = infiniteDeadline
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string { // comparar fechas y no desplegar las vencidas
+	allKeys := []string{}
+	for key := range c.cachedValues {
+		now := time.Now()
+		if now.Before(c.deadlines[key]) {
+			allKeys = append(allKeys, key)
+		} else {
+			delete(c.cachedValues, key)
+			delete(c.deadlines, key)
+		}
+	}
+	return allKeys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	now := time.Now()
+	if now.Before(deadline) {
+		c.deadlines[key] = deadline
+		c.cachedValues[key] = value
+	}
 }
